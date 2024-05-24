db7801a465087950b90a6499ae13e0de06fd17bb
hanbiao <hanbiao4079@gmail.com>
2024-05-24 16:43:51 +0800

    update markdown

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,9 @@ mod templates;
 
 use alter::Alter;
 use anyhow::Ok;
-use markdown::{parse_doc_file, read_temp_html, DocInfoList};
 
 fn main() -> Result<(), anyhow::Error> {
-    // let alter = Alter::new();
-    // alter.init();
-
-    read_temp_html();
-
+    let alter = Alter::new();
+    alter.init();
     Ok(())
 }
